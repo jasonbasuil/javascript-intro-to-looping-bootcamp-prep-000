800fc9a662003a8ba7d161ce827af424bb136e33
Jason Basuil <basuilj@gmail.com>
2018-10-11 17:41:28 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array){
+  for (let i = 0; i <25; i++){
+    if (i == 1) {
+      array.unshift('I am 1 strange loop.');
+    }
+    else {
+      array.unshift(`"I am ${i} strange loop"`);
+    }
+}}
+
+function whileLoop (number) {
+  let countdown = number
+  while (countdown > 0){
+    console.log(--countdown);
+  return console.log('done')
+  }
+}
+
+function doWhileLoop (array) {
+  do {
+    [loopBody]
+  } while ([condition]);
+}
